2351eb5b669490307366f596ee925f3523702a84
Rajneesh058 <114851821+Rajneesh058@users.noreply.github.com>
2023-06-12 23:10:07 +0530

    Update start_help.py

--- Adarsh/bot/plugins/start_help.py
+++ Adarsh/bot/plugins/start_help.py
@@ -180,7 +180,7 @@ async def help_handler(bot, message):
                             [
                                 InlineKeyboardButton("ᴊᴏɪɴ ᴏᴜʀ ᴜᴩᴅᴀᴛᴇꜱ ᴄʜᴀɴɴᴇʟ", url=f"https://t.me/{Var.UPDATES_CHANNEL}")
                      ],[
-                     InlineKeyboardButton(" 🔄 Try Again", url=f"https://t.me/{temp.U_NAME}?start={message.command[1]}")]
+                                InlineKeyboardButton(" 🔄 Try Again", callback_data='start')]
                      ]
                     )
                     
