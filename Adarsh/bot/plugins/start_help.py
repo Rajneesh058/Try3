ae1a3f20da3f5f10b70f40a78e4257cb66fdec97
Rajneesh058 <114851821+Rajneesh058@users.noreply.github.com>
2023-06-18 13:38:32 +0530

    Update start_help.py

--- Adarsh/bot/plugins/start_help.py
+++ Adarsh/bot/plugins/start_help.py
@@ -12,6 +12,7 @@ from pyrogram.errors import UserNotParticipant
 from Adarsh.utils.file_properties import get_name, get_hash, get_media_file_size
 db = Database(Var.DATABASE_URL, Var.name)
 from pyrogram.types import ReplyKeyboardMarkup
+from pyrogram.enums.parse_mode import ParseMode
 
 START_BUTTONS = InlineKeyboardMarkup(
         [[
