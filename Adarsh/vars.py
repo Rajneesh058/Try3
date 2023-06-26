9883bac9c5b94b72fb7e7cba5e9c0b0e138207aa
Rajneesh058 <114851821+Rajneesh058@users.noreply.github.com>
2023-06-26 12:14:56 +0530

    Update vars.py

--- Adarsh/vars.py
+++ Adarsh/vars.py
@@ -38,4 +38,4 @@ class Var(object):
     DATABASE_URL = str(getenv('DATABASE_URL'))
     UPDATES_CHANNEL = str(getenv('UPDATES_CHANNEL', None))
     BANNED_CHANNELS = list(set(int(x) for x in str(getenv("BANNED_CHANNELS", "-1001362659779")).split())) 
-    SHORT_URL = str(getenv('SHORT_URL', ""))
+    SHORT_URL = str(getenv('SHORT_URL', "Afly.in"))
